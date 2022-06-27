a6d6b0802c63656ee2fec40c0044b785b6773cfa
sboursen <dev.sboursen@gmail.com>
2022-06-27 15:09:53 +0100

    Test the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Kaladin', email: 'kaladin@roshar.cos', password: '0123456789') }
+
+  describe 'user' do
+    it 'should save when all parameters are valid' do
+      subject.save
+      expect(User.first.name).to eq('Kaladin')
+    end
+  end
+
+  describe 'name' do
+    it 'should be non empty' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have a max length of 250' do
+      subject.name = 251.times.reduce('') { |t, _i| t + '9' }
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'email' do
+    it 'should be non empty' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should be valid email format' do
+      subject.email = 'hoid?roshar.cos'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'password' do
+    it 'should be non empty' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have at last 6 chars' do
+      subject.password = '01234'
+      expect(subject).to_not be_valid
+    end
+  end
 end
