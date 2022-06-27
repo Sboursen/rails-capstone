b216cc2ead119a32c0f039d8ba5eba94511ff228
sboursen <dev.sboursen@gmail.com>
2022-06-27 16:37:50 +0100

    Add color palette

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#3678c2',
+
+          secondary: '#60b524',
+
+          accent: '#878a8f',
+
+          neutral: '#434c55',
+
+          'base-100': '#FFFFFF',
+
+          info: '#3678c2',
+
+          success: '#60b524',
+
+          warning: '#FBBD23',
+
+          error: '#F87272',
+        },
+      },
+    ],
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
