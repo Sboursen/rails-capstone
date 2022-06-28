ce38b1a887d56b75307f9c3254678cb3cb156e8c
sboursen <dev.sboursen@gmail.com>
2022-06-28 16:42:22 +0100

    Create the categories#create and categories#index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,23 @@ class CategoriesController < ApplicationController
     @categories = @current_user.categories
   end
 
+  def new; end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author_id = current_user.id
+    if @category.save
+      flash[:notice] = 'Your category was successfully created'
+      redirect_to categories_path
+    else
+      flash[:notice] = 'Failed to create the category!'
+      render 'new'
+    end
+  end
+
   private
 
-  def categories_params
+  def category_params
     params.require(:category).permit(:name, :icon)
   end
 end
