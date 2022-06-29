bbd130701eba18784f012e51492bc58c63739c2f
sboursen <dev.sboursen@gmail.com>
2022-06-29 17:32:40 +0100

    Fix turbo-devise incompatibility

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+
+const flashContainer = document.getElementById("flash-container");
+
+flashContainer.addEventListener('click', (e) => {
+  if(e.currentTarget.id === "flash-container") {
+    e.target.innerHTML = '';
+  }
+});
