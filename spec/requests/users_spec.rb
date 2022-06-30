e3e2d0fecefa884ff2907e0c68557963ef95671b
sboursen <dev.sboursen@gmail.com>
2022-06-30 09:30:48 +0100

    Add users/sign_in spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Users', type: :request do
       expect(response.body.include?('Portefeuille')).to be_truthy
     end
   end
+
+  describe 'GET #sign_in' do
+    before(:example) { get new_user_session_path }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('devise/sessions/new')
+    end
+
+    it 'response body includes title' do
+      expect(response.body.include?('LOGIN')).to be_truthy
+    end
+  end
 end
